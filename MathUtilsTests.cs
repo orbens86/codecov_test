a2450fef1b529fefebeb7f0702044bddb4c03a29
orbens86 <or.ben-shmueli@pathlock.com>
2025-06-30 16:21:50 +0300

    Add files via upload

---
+++ MathUtilsTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+
+public class MathUtilsTests
+{
+    [Fact]
+    public void Add_ReturnsCorrectSum()
+    {
+        Assert.Equal(5, MathUtils.Add(2, 3));
+    }
+
+    [Fact]
+    public void Subtract_ReturnsCorrectDifference()
+    {
+        Assert.Equal(1, MathUtils.Subtract(3, 2));
+    }
+}
+
+
+public class StringUtilsTests
+{
+    [Fact]
+    public void Reverse_ReturnsReversedString()
+    {
+        Assert.Equal("tac", StringUtils.Reverse("cat"));
+    }
+}
